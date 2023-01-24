73c5760a34a08ad0fec4a2753b9b4862ffadaa53
Silvio Arem <silvio.arem@gmail.com>
2023-01-23 21:36:18 -0300

    App.tsx estruturado

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
+import TaskContainer from "./components/LinkContainer";
+import TaskItem from "./components/TaskItem";
+
 import "./styles/global.css"
 
 function App() {
   return (
-    <main>
-      <h1></h1>
-      <div>
-        <button>
-          
+    <main className="container">
+      <h1 className="m-5">📋 Task Manager</h1>
+      <div id="new-task">
+        <button id="btnAdd" className="btn btn-primary">
+          Add
         </button>
       </div>
+      <TaskContainer>
+        <TaskItem titulo="tarefa 1" />
+        <TaskItem titulo="tarefa-2" done={true} />
+      </TaskContainer>
     </main>
   );
 }
