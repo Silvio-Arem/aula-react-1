ab0e08fb9aeddae8085b6ff5d1bc41dca5095176
Silvio Arem <silvio.arem@gmail.com>
2023-01-26 13:42:24 -0300

    componente de estilo do botão criado

---
+++ src/components/Botao/styles.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const BotaoDefault = styled.button`
+    border: none;
+    background-color: green;
+    color: #fff;
+    border-radius: 20px;
+    padding: 10px 30px;
+`;
