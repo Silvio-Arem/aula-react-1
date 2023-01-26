1eb895b3cb7c68bc0cabc745abf962760a0ac845
Silvio Arem <silvio.arem@gmail.com>
2023-01-26 14:14:22 -0300

    incluida a props cor no componente Botao

--- src/components/Botao/index.tsx
+++ src/components/Botao/index.tsx
@@ -2,11 +2,12 @@ import {BotaoDefault} from "./styles";
 
 interface BotaoProps {
     texto: string;
+    cor: string;
     acao?: () => void;
 }
 
 export default function Botao(props: BotaoProps) {
     return (
-        <BotaoDefault onClick={props.acao}>{props.texto}</BotaoDefault>
+        <BotaoDefault cor={props.cor} onClick={props.acao}>{props.texto}</BotaoDefault>
     )
 }
